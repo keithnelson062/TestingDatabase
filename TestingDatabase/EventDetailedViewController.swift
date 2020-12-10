218d1289a553e88dbcefd543ef400364374e2436
Michelle Katz <michellekatz@wustl.edu>
2020-12-10 16:30:17 -0600

    styling changes

--- TestingDatabase/EventDetailedViewController.swift
+++ TestingDatabase/EventDetailedViewController.swift
@@ -19,16 +19,14 @@ class EventDetailedViewController: UIViewController {
     @IBOutlet weak var eventName: UILabel!
     @IBOutlet weak var eventDescription: UITextView!
     @IBOutlet weak var location: UITextView!
-    @IBOutlet weak var contactInfo: UITextView!
     @IBOutlet weak var timeDate: UITextView!
     @IBOutlet weak var covidPrecautions: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        eventName.text = "Name: " + name
+        eventName.text = name
         eventDescription.text = "Description: " + desc
         location.text = "Location: " + loc
-        contactInfo.text = "Contact Info: " + contact
         timeDate.text = "Date/Time: " + td
         covidPrecautions.text = "COVID-19 Precautions: " + covid
 
