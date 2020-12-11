26431b3063571ce4845c4b712e3cb7e0545425de
Michelle Katz <michellekatz@wustl.edu>
2020-12-11 00:21:12 -0600

    Updated style

--- TestingDatabase/ConnectionViewCell.swift
+++ TestingDatabase/ConnectionViewCell.swift
@@ -19,22 +19,24 @@ class ConnectionViewCell: UICollectionViewCell {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        let nameFrame = CGRect(x: 4, y: 10, width: 345, height: 25)
+        let nameFrame = CGRect(x: 4, y: 10, width: 3375, height: 25)
         cName = UILabel(frame: nameFrame)
         cName.textColor = UIColor.black
-        cName.font = UIFont(name: "Avenir Next", size: 14)
+        cName.font = UIFont(name: "Avenir Next", size: 22)
         contentView.addSubview(cName)
-        let descFrame = CGRect(x: 0, y: 33, width: 345, height: 64)
+        let descFrame = CGRect(x: 0, y: 45, width: 360, height: 60)
         cDescription = UITextView(frame: descFrame)
         cDescription.textColor = UIColor.black
-        cDescription.font = UIFont(name: "Avenir Next", size: 14)
+        cDescription.font = UIFont(name: "Avenir Next", size: 18)
         cDescription.isEditable = false
+        cDescription.backgroundColor = UIColor(named: "VeryLightBlue")
         contentView.addSubview(cDescription)
-        let cvmFrame = CGRect(x: 4, y: 94, width: 345, height: 25)
+        let cvmFrame = CGRect(x: 4, y: 100, width: 375, height: 30)
         clickViewMore = UILabel(frame: cvmFrame)
         contentView.addSubview(clickViewMore)
         clickViewMore.textColor = UIColor.black
-        clickViewMore.font = UIFont(name: "Avenir Next", size: 12)
+        clickViewMore.font = UIFont(name: "Avenir Next", size: 14)
+        contentView.backgroundColor = UIColor(named: "VeryLightBlue")
     }
     
     func setCell(name: String, description: String) {
